163290cd8a293aa6153ef6958b16825199d9e8cd
Ewuranna Smith-Quayson <qusmew@gmail.com>
2023-07-08 13:22:52 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<div class="min-h-screen flex items-center justify-center bg-black">
+
+    <div class="bg-white p-8 rounded-lg w-full max-w-md"> 
+
+       <div class=" flex flex-col items-center mb-6 rounded-md">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="src/images/me.jpg" alt="my profile picture" class="w-24 h-24 rounded-full object-cover drop-shadow-lg" >
+        <h1 class="text-2xl font-semibold my-5"> Ewuranna Smith-Quayson</h1>
+        <p class="text-center text-gray-600 mb-2"> Digital Technology Specialist | Passionate about driving innovation and leveraging technology to create positive change</p>
+        
+        <ul class=" space-y-4 ">
+            <li >
+                <a href="https://ewuranna.com" target="_blank" class= "bg-black drop-shadow-lg rounded-md mt-6 w-full block py-3 px-24 text-center text-white" > Website</a>
+            </li>
+
+            <li >
+                <a href="https://twitter.com/ewuranna" target="_blank" class= "bg-black drop-shadow-lg rounded-md mt-6 w-full block py-3 px-24 text-center text-white" > Twitter</a>
+            </li>
+            <li >
+                <a href="https://ewuranna.medium.com" target="_blank" class= "bg-black drop-shadow-lg rounded-md mt-6 w-full block py-3 px-24 text-center text-white" > Medium</a>
+            </li>
+            <li >
+                <a href="https://www.linkedin.com/in/ewuranna-smith-quayson-92bb4a6b/" target="_blank" class= "bg-black drop-shadow-lg rounded-md mt-6 w-full block py-3 px-24 text-center text-white" > LinkedIn</a>
+            </li>
+
+            <li >
+                <a href="https://www.threads.net/@mssmithquayson " target="_blank" class= "bg-black drop-shadow-lg rounded-md mt-6 w-full block py-2 px-24 text-center text-white" > Threads</a>
+            </li>
+
+        </ul>
+        
+        
+      
+
+   
+   
+   
+    </div>
+   
+    </div>
+
+</div>
